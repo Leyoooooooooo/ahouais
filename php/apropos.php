d0a1b219e10b0efbc14dc550129bba997e75f95d
Léo <90392466+Leyoooooooooo@users.noreply.github.com>
2021-12-17 14:51:41 +0100

    pdezolf
    oezbihfj

--- php/apropos.php
+++ php/apropos.php
@@ -4,8 +4,8 @@ $data=yaml_parse_file("data/apropos.yaml");
 <h1> A PROPOS </h1>
 <hr id="barre">
 <?php
-echo '<h1>'.$data["accroche"].'</h1>';
-echo '<h1>'.$data["presentation"].'</h1>';
+echo '<h2>'.$data["accroche"].'</h1>';
+echo '<p>'.$data["presentation"].'</p>';
 echo '<img id="imgpropos" src="'.$data['image'].'">';
 
 ?>
