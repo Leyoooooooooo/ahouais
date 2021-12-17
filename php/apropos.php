d49c87440b4af3e6b821d0cfb24d35007397bbed
Léo <90392466+Leyoooooooooo@users.noreply.github.com>
2021-12-17 15:06:22 +0100

    fin cour

--- php/apropos.php
+++ php/apropos.php
@@ -4,8 +4,10 @@ $data=yaml_parse_file("data/apropos.yaml");
 <h1> A PROPOS </h1>
 <hr id="barre">
 <?php
-echo '<h2>'.$data["accroche"].'</h1>';
+echo '<h2>'.$data["accroche"].'</h2>';
 echo '<p>'.$data["presentation"].'</p>';
 echo '<img id="imgpropos" src="'.$data['image'].'">';
 
 ?>
+
+<br>
