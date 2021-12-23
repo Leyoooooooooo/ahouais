7cf9dc3b47f2ba43ab4b239fede0be9364a4ad5c
Léo <90392466+Leyoooooooooo@users.noreply.github.com>
2021-12-23 15:27:50 +0100

    Update experience.php

--- php/experience.php
+++ php/experience.php
@@ -10,8 +10,9 @@ $data=yaml_parse_file("data/experience.yaml");
 
 echo '<p>Stage de '.$data["Duree1"].' chez ' .$data["entreprise1"].' en '.$data["Annee1"].'.('.$data["Taches1"].' de materiels informatique.) </p>';
 
-echo '<p>Stage de '.$data["Duree2"].' à la fédération de la Manche de '.$data["entreprise2"].' en '.$data["Annee2"].'.('.$data["Taches2"].'</p>';
+echo '<p>Stage de '.$data["Duree2"].' à la fédération de la Manche de '.$data["entreprise2"].' en '.$data["Annee2"].'.('.$data["Taches2"].')</p>';
 
+echo '<p>Stage de '.$data["Duree3"].' au ' .$data["entreprise3"].' en '.$data["Annee3"].'.('.$data["Taches3"].') </p>';
 
 
 
