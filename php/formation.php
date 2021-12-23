c8200bef34a7acd463f46d97eb649d42df4e1d9d
Léo <90392466+Leyoooooooooo@users.noreply.github.com>
2021-12-23 17:17:46 +0100

    xx

--- php/formation.php
+++ php/formation.php
@@ -7,11 +7,13 @@ $data=yaml_parse_file("data/formation.yaml");
 
 <?php
 
-echo '<p> Diplôme du '.$data["diplome1"].' au '.$data["etablissement1"].' à '.$data["lieu1"].' en '.$data["duree1"].'</p>';
+echo '<p> Diplôme du '.$data["diplome1"].' au '.$data["etablissement1"].' à '.$data["lieu1"].' en '.$data["duree1"].'.</p>';
 
-echo '<p> Diplôme du '.$data["diplome2"].' au '.$data["etablissement2"].' à '.$data["lieu2"].' en '.$data["duree2"].'</p>';
+echo '<p> Diplôme du '.$data["diplome2"].' au '.$data["etablissement2"].' à '.$data["lieu2"].' en '.$data["duree2"].'.</p>';
 
-echo '<p> Diplôme du '.$data["diplome3"].' au '.$data["etablissement2"].' à '.$data["lieu2"].' en '.$data["duree3"].'</p>';
+echo '<p> Diplôme du '.$data["diplome3"].' au '.$data["etablissement2"].' à '.$data["lieu2"].' en '.$data["duree3"].'.</p>';
+
+echo '<p> Actuellement en préparation du diplôme '.$data["diplome4"].' a '.$data["etablissement3"].' à '.$data["lieu3"].'.</p>';
 
 
 
