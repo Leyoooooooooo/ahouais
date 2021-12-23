a7270bd5f386eb7442ade1caf1a17226e0cc85ce
Léo <90392466+Leyoooooooooo@users.noreply.github.com>
2021-12-23 16:51:48 +0100

    c

--- php/formation.php
+++ php/formation.php
@@ -9,7 +9,7 @@ $data=yaml_parse_file("data/formation.yaml");
 
 echo '<p> Diplôme du '.$data["diplome1"].' au '.$data["etablissement1"].' à '.$data["lieu1"].' en '.$data["duree1"].'</p>';
 
-
+echo '<p> Diplôme du '.$data["diplome2"].' au '.$data["etablissement2"].' à '.$data["lieu2"].' en '.$data["duree2"].'</p>';
 
 
 
